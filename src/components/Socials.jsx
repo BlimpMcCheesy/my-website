a06f50dcf79dc857a2266b11e96ce7951718579f
Kyle Whitman <131239798+BlimpMcCheesy@users.noreply.github.com>
2025-08-07 22:55:32 -0400

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,7 +5,7 @@ import './Socials.css';
 function Socials() {
   return (
     <div className="socials-container">
-      <a href="https://github.com/BlimpMcheesy" className="social-button" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+      <a href="https://github.com/BlimpMcCheesy" className="social-button" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
         <FaGithub size={24} />
       </a>
       <a href="https://linkedin.com/in/kyle-whitman1" className="social-button" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
